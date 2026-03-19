8b449d0b8dbb54f2dbd8028a741745e197ec1e51
aamnaakhtar <aamnaakhtar7@gmai.com>
2026-03-19 23:53:21 +0530

    Deployed e0a6728

--- assets/utility-hub.js
+++ assets/utility-hub.js
@@ -282,41 +282,41 @@
   _exports.default = Router;
   Router.map(function () {});
 });
-;define("utility-hub/routes/index", ["exports", "@ember/routing/route"], function (_exports, _route) {
+;define("utility-hub/routes/index", ["exports", "@ember/routing/route", "utility-hub/config/environment"], function (_exports, _route, _environment) {
   "use strict";
 
   Object.defineProperty(_exports, "__esModule", {
     value: true
   });
   _exports.default = void 0;
-  0; //eaimeta@70e063a35619d71f0,"@ember/routing/route"eaimeta@70e063a35619d71f
+  0; //eaimeta@70e063a35619d71f0,"@ember/routing/route",0,"utility-hub/config/environment"eaimeta@70e063a35619d71f
   class IndexRoute extends _route.default {
     model() {
       return [{
         title: 'Basic To Do List App',
         description: 'Manage your tasks, stay organized and boost your productivity with our intuitive to-do list app.',
         link: 'https://aamnaakhtar.github.io/to-do-list-js',
-        image: `${config.rootURL}assets/todo.jpg`
+        image: `${_environment.default.rootURL}assets/todo.jpg`
       }, {
         title: 'Weather App',
         description: 'Check the current weather of anywhere in one click',
         link: 'https://aamnaakhtar.github.io/weather-app',
-        image: `${config.rootURL}assets/weather.jpg`
+        image: `${_environment.default.rootURL}assets/weather.jpg`
       }, {
         title: 'Currency Converter',
         description: 'Check the real-time accurate exchange rates with a user-friendly interface.',
         link: 'https://aamnaakhtar.github.io/currency-converter',
-        image: `${config.rootURL}assets/currency-convertor.jpg`
+        image: `${_environment.default.rootURL}assets/currency-convertor.jpg`
       }, {
         title: 'QR Generator',
         description: 'Generate QR codes for any text or URL with our simple and efficient QR code generator.',
         link: 'https://aamnaakhtar.github.io/qrGenerator',
-        image: `${config.rootURL}assets/qrcode.jpg`
+        image: `${_environment.default.rootURL}assets/qrcode.jpg`
       }, {
         title: 'Quote Of The Day',
         description: 'Get inspirational and motivational quote on one click',
         link: 'https://aamnaakhtar.github.io/quotes-generator',
-        image: `${config.rootURL}assets/quote.jpg`
+        image: `${_environment.default.rootURL}assets/quote.jpg`
       }];
     }
   }
